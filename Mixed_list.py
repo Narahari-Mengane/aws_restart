aca3fb72853a3ee9dc656760fee85b136b3ef0f1
Narahari Mengane <naraharimengane51@gmail.com>
2023-04-03 22:31:41 +0530

    Delete Mixed_list.py

--- Mixed_list.py
+++
@@ -1,5 +0,0 @@
-myMixedTypeList = [45, 290578, 1.02, True, "My dog is on the bed.", "45"]
-print(myMixedTypeList)
-for item in myMixedTypeList:
-    print("\n{} is of the data type {}".format(item,type(item)))
-    
